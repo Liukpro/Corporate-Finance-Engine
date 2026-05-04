0875f3c9f02f88c2e9ad7a6952d54d1e39ea5d20
Luca di Summa <149318306+Liukpro@users.noreply.github.com>
2026-05-04 18:12:07 +0200

    Update executioner.py

--- executioner.py
+++ executioner.py
@@ -1,3 +1,17 @@
-from yahoo_fetcher import fetch_financials, fetch_report
-result = fetch_financials("RACE", year=2023)
-fetch_report(result)
+from yahoo_fetcher import fetch_financials
+from dag_resolver import resolve_inputs
+from dag_calculator import *
+
+def main(ticker):
+
+    raw = fetch_financials(ticker)
+
+    data = resolve_inputs(raw)
+
+    mol = calc_mol(data["ric_op_mon"], data["cost_op_mon"])
+    rol = calc_rol_from_mol(mol, data["ammort"])
+
+    print(mol, rol)
+
+if __name__ == "__main__":
+    main("RACE")
